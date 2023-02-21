5b02c3b9c337a406a2581952337a293ebe2c7eb8
yoonseok2 <yoonseok236@gmail.com>
2023-02-21 23:47:45 +0900

    test

---
+++ src/main/java/com/gryard/basecamp/BasecampApplication.java
@@ -0,0 +1,13 @@
+package com.gryard.basecamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasecampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasecampApplication.class, args);
+	}
+
+}
